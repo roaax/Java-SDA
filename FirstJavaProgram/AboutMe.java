cf8aaf229bb3baa9071e8b2e41ac07b560e633b7
Roaa Albishri <106008427+roaax@users.noreply.github.com>
2022-07-17 07:26:39 +0300

    Add files via upload

---
+++ FirstJavaProgram/AboutMe.java
@@ -0,0 +1,7 @@
+public class AboutMe {
+	public static void main(String[] args) {
+		System.out.println("My Name is Roaa");
+        System.out.println("I am 23 years old");
+        System.out.println("My hometown is Jeddah, Saudi Arabia");
+	}
+}
