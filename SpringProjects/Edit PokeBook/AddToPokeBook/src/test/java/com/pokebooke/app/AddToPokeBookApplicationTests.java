d553fd600c69e4a6abf69172b1f34abafc57026c
Roaa Albishri <106008427+roaax@users.noreply.github.com>
2022-07-31 09:57:35 +0300

    Edit Book Assignment

---
+++ SpringProjects/Edit PokeBook/AddToPokeBook/src/test/java/com/pokebooke/app/AddToPokeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokebooke.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddToPokeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
